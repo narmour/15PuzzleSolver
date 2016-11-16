147e3b68c4923d670fecc9c758c8a12aa016e4c8
narmour <n.armour0@gmail.com>
2016-11-15 19:52:09 -0800

    first commit

---
+++ app/src/main/java/threesixty/a15puzzle/MainMenuFragment.java
@@ -0,0 +1,40 @@
+package threesixty.a15puzzle;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainMenuFragment extends Fragment {
+    private Button playButton;
+
+
+    public MainMenuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main_menu, container, false);
+        //set the buttons
+        playButton = (Button)view.findViewById(R.id.playButton);
+
+
+        // set on click listeners
+
+
+
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+
+
+}
---
+++ app/src/main/java/threesixty/a15puzzle/Startup.java
@@ -0,0 +1,23 @@
+package threesixty.a15puzzle;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Startup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+
+        // load main menu fragment
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        MainMenuFragment MMF = new MainMenuFragment();
+        ft.replace(R.id.fragment_container, MMF, null);
+        ft.addToBackStack("MMF");
+        ft.commit();
+    }
+}
